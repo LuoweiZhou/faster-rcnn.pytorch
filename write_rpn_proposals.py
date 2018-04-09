954ff0e7a2b41534baf19c7f625d7fcb540f2900
Luowei Zhou <luozhou@zoom.eecs.umich.edu>
2018-04-09 15:26:25 -0400

    write proposals to file

---
+++ write_rpn_proposals.py
@@ -0,0 +1,367 @@
+# --------------------------------------------------------
+# Tensorflow Faster R-CNN
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Jiasen Lu, Jianwei Yang, based on code from Ross Girshick
+# --------------------------------------------------------
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import _init_paths
+import os
+import sys
+import numpy as np
+import argparse
+import pprint
+import pdb
+import time
+import cv2
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.optim as optim
+from collections import defaultdict
+import json
+
+import torchvision.transforms as transforms
+import torchvision.datasets as dset
+from scipy.misc import imread
+from roi_data_layer.roidb import combined_roidb
+from roi_data_layer.roibatchLoader import roibatchLoader
+from model.utils.config import cfg, cfg_from_file, cfg_from_list, get_output_dir
+from model.rpn.bbox_transform import clip_boxes
+from model.nms.nms_wrapper import nms
+from model.rpn.bbox_transform import bbox_transform_inv
+from model.utils.net_utils import save_net, load_net, vis_detections
+from model.utils.blob import im_list_to_blob
+from model.faster_rcnn.vgg16 import vgg16
+from model.faster_rcnn.resnet import resnet
+import pdb
+
+try:
+    xrange          # Python 2
+except NameError:
+    xrange = range  # Python 3
+
+
+def parse_args():
+  """
+  Parse input arguments
+  """
+  parser = argparse.ArgumentParser(description='Train a Fast R-CNN network')
+  parser.add_argument('--dataset', dest='dataset',
+                      help='training dataset',
+                      default='pascal_voc', type=str)
+  parser.add_argument('--cfg', dest='cfg_file',
+                      help='optional config file',
+                      default='cfgs/vgg16.yml', type=str)
+  parser.add_argument('--net', dest='net',
+                      help='vgg16, res50, res101, res152',
+                      default='res101', type=str)
+  parser.add_argument('--set', dest='set_cfgs',
+                      help='set config keys', default=None,
+                      nargs=argparse.REMAINDER)
+  parser.add_argument('--load_dir', dest='load_dir',
+                      help='directory to load models',
+                      default="/srv/share/jyang375/models")
+  parser.add_argument('--image_dir', dest='image_dir',
+                      help='directory to load images for demo',
+                      default="images")
+  parser.add_argument('--target_dir', dest='target_dir',
+                      help='directory to load images for demo',
+                      default="images_det")
+  parser.add_argument('--cuda', dest='cuda',
+                      help='whether use CUDA',
+                      action='store_true')
+  parser.add_argument('--mGPUs', dest='mGPUs',
+                      help='whether use multiple GPUs',
+                      action='store_true')
+  parser.add_argument('--cag', dest='class_agnostic',
+                      help='whether perform class_agnostic bbox regression',
+                      action='store_true')
+  parser.add_argument('--parallel_type', dest='parallel_type',
+                      help='which part of model to parallel, 0: all, 1: model before roi pooling',
+                      default=0, type=int)
+  parser.add_argument('--checksession', dest='checksession',
+                      help='checksession to load model',
+                      default=1, type=int)
+  parser.add_argument('--checkepoch', dest='checkepoch',
+                      help='checkepoch to load network',
+                      default=1, type=int)
+  parser.add_argument('--checkpoint', dest='checkpoint',
+                      help='checkpoint to load network',
+                      default=10021, type=int)
+  parser.add_argument('--bs', dest='batch_size',
+                      help='batch_size',
+                      default=1, type=int)
+  parser.add_argument('--vis', dest='vis',
+                      help='visualization mode',
+                      action='store_true')
+  parser.add_argument('--max_per_image', dest='max_per_image',
+                      help='max bounding boxes per image',
+                      default=100, type=int)
+  parser.add_argument('--webcam_num', dest='webcam_num',
+                      help='webcam ID number',
+                      default=-1, type=int)
+  parser.add_argument('--ls', dest='large_scale',
+                      help='whether use large imag scale',
+                      action='store_true')
+  parser.add_argument('--output_lst', default='/z/dat/yc2/segment_thumbnail_all_split_1fps/testing_box.txt')
+  parser.add_argument('--output_chunk', default='/z/dat/yc2/segment_thumbnail_all_split_1fps/testing_box.pth')
+
+  args = parser.parse_args()
+  return args
+
+lr = cfg.TRAIN.LEARNING_RATE
+momentum = cfg.TRAIN.MOMENTUM
+weight_decay = cfg.TRAIN.WEIGHT_DECAY
+
+def _get_image_blob(im):
+  """Converts an image into a network input.
+  Arguments:
+    im (ndarray): a color image in BGR order
+  Returns:
+    blob (ndarray): a data blob holding an image pyramid
+    im_scale_factors (list): list of image scales (relative to im) used
+      in the image pyramid
+  """
+  im_orig = im.astype(np.float32, copy=True)
+  im_orig -= cfg.PIXEL_MEANS
+
+  im_shape = im_orig.shape
+  im_size_min = np.min(im_shape[0:2])
+  im_size_max = np.max(im_shape[0:2])
+
+  processed_ims = []
+  im_scale_factors = []
+
+  for target_size in cfg.TEST.SCALES:
+    im_scale = float(target_size) / float(im_size_min)
+    # Prevent the biggest axis from being more than MAX_SIZE
+    if np.round(im_scale * im_size_max) > cfg.TEST.MAX_SIZE:
+      im_scale = float(cfg.TEST.MAX_SIZE) / float(im_size_max)
+    im = cv2.resize(im_orig, None, None, fx=im_scale, fy=im_scale,
+            interpolation=cv2.INTER_LINEAR)
+    im_scale_factors.append(im_scale)
+    processed_ims.append(im)
+
+  # Create a blob to hold the input images
+  blob = im_list_to_blob(processed_ims)
+
+  return blob, np.array(im_scale_factors)
+
+if __name__ == '__main__':
+
+  args = parse_args()
+
+  print('Called with args:')
+  print(args)
+
+  if args.dataset == "pascal_voc":
+      args.imdb_name = "voc_2007_trainval"
+      args.imdbval_name = "voc_2007_test"
+      args.set_cfgs = ['ANCHOR_SCALES', '[8, 16, 32]', 'ANCHOR_RATIOS', '[0.5,1,2]', 'MAX_NUM_GT_BOXES', '20']
+  elif args.dataset == "pascal_voc_0712":
+      args.imdb_name = "voc_2007_trainval+voc_2012_trainval"
+      args.imdbval_name = "voc_2007_test"
+      args.set_cfgs = ['ANCHOR_SCALES', '[8, 16, 32]', 'ANCHOR_RATIOS', '[0.5,1,2]', 'MAX_NUM_GT_BOXES', '20']
+  elif args.dataset == "coco":
+      args.imdb_name = "coco_2014_train+coco_2014_valminusminival"
+      args.imdbval_name = "coco_2014_minival"
+      args.set_cfgs = ['ANCHOR_SCALES', '[4, 8, 16, 32]', 'ANCHOR_RATIOS', '[0.5,1,2]', 'MAX_NUM_GT_BOXES', '50']
+  elif args.dataset == "imagenet":
+      args.imdb_name = "imagenet_train"
+      args.imdbval_name = "imagenet_val"
+      args.set_cfgs = ['ANCHOR_SCALES', '[4, 8, 16, 32]', 'ANCHOR_RATIOS', '[0.5,1,2]', 'MAX_NUM_GT_BOXES', '30']
+  elif args.dataset == "vg":
+      # train sizes: train, smalltrain, minitrain
+      # train scale: ['150-50-20', '150-50-50', '500-150-80', '750-250-150', '1750-700-450', '1600-400-20']
+      args.imdb_name = "vg_150-50-50_minitrain"
+      args.imdbval_name = "vg_150-50-50_minival"
+      args.set_cfgs = ['ANCHOR_SCALES', '[4, 8, 16, 32]', 'ANCHOR_RATIOS', '[0.5,1,2]', 'MAX_NUM_GT_BOXES', '50']
+
+  args.cfg_file = "cfgs/{}_ls.yml".format(args.net) if args.large_scale else "cfgs/{}.yml".format(args.net)
+
+  if args.cfg_file is not None:
+    cfg_from_file(args.cfg_file)
+  if args.set_cfgs is not None:
+    cfg_from_list(args.set_cfgs)
+
+  print('Using config:')
+  pprint.pprint(cfg)
+  np.random.seed(cfg.RNG_SEED)
+
+  # train set
+  # -- Note: Use validation set and disable the flipped to enable faster loading.
+
+  input_dir = args.load_dir + "/" + args.net + "/" + args.dataset
+  if not os.path.exists(input_dir):
+    raise Exception('There is no input directory for loading network from ' + input_dir)
+  load_name = os.path.join(input_dir,
+    'faster_rcnn_{}_{}_{}.pth'.format(args.checksession, args.checkepoch, args.checkpoint))
+
+  if args.dataset == 'pascal_voc':
+      pascal_classes = np.asarray(['__background__',
+                           'aeroplane', 'bicycle', 'bird', 'boat',
+                           'bottle', 'bus', 'car', 'cat', 'chair',
+                           'cow', 'diningtable', 'dog', 'horse',
+                           'motorbike', 'person', 'pottedplant',
+                           'sheep', 'sofa', 'train', 'tvmonitor'])
+  elif args.dataset == 'coco':
+      pascal_classes = np.asarray(['__background__',
+                           'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
+                           'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+                           'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
+                           'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard',
+                           'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+                           'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
+                           'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard',
+                           'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase',
+                           'scissors', 'teddy bear', 'hair drier', 'toothbrush'])
+  else:
+      raise Exception('not implemented!')
+
+  # initilize the network here.
+  if args.net == 'vgg16':
+    fasterRCNN = vgg16(pascal_classes, pretrained=False, class_agnostic=args.class_agnostic)
+  elif args.net == 'res101':
+    fasterRCNN = resnet(pascal_classes, 101, pretrained=False, class_agnostic=args.class_agnostic)
+  elif args.net == 'res50':
+    fasterRCNN = resnet(pascal_classes, 50, pretrained=False, class_agnostic=args.class_agnostic)
+  elif args.net == 'res152':
+    fasterRCNN = resnet(pascal_classes, 152, pretrained=False, class_agnostic=args.class_agnostic)
+  else:
+    print("network is not defined")
+    pdb.set_trace()
+
+  fasterRCNN.create_architecture()
+
+  print("load checkpoint %s" % (load_name))
+  checkpoint = torch.load(load_name)
+  fasterRCNN.load_state_dict(checkpoint['model'])
+  if 'pooling_mode' in checkpoint.keys():
+    cfg.POOLING_MODE = checkpoint['pooling_mode']
+
+
+  print('load model successfully!')
+
+  # pdb.set_trace()
+
+  print("load checkpoint %s" % (load_name))
+
+  # initilize the tensor holder here.
+  im_data = torch.FloatTensor(1)
+  im_info = torch.FloatTensor(1)
+  num_boxes = torch.LongTensor(1)
+  gt_boxes = torch.FloatTensor(1)
+
+  # ship to cuda
+  if args.cuda > 0:
+    im_data = im_data.cuda()
+    im_info = im_info.cuda()
+    num_boxes = num_boxes.cuda()
+    gt_boxes = gt_boxes.cuda()
+
+  # make variable
+  im_data = Variable(im_data, volatile=True)
+  im_info = Variable(im_info, volatile=True)
+  num_boxes = Variable(num_boxes, volatile=True)
+  gt_boxes = Variable(gt_boxes, volatile=True)
+
+  if args.cuda > 0:
+    cfg.CUDA = True
+
+  if args.cuda > 0:
+    fasterRCNN.cuda()
+
+  fasterRCNN.eval()
+
+  start = time.time()
+  max_per_image = args.max_per_image
+  thresh = 0.05
+  vis = args.vis
+
+  webcam_num = args.webcam_num
+  # Set up webcam or get image directories
+  if webcam_num >= 0 :
+    cap = cv2.VideoCapture(webcam_num)
+    num_images = 0
+  else:
+    imglist = os.listdir(args.image_dir)
+    num_images = len(imglist)-1
+
+  print('Loaded Photo: {} images.'.format(num_images+1))
+
+  results = []
+  while (num_images >= 0):
+      total_tic = time.time()
+
+      # Get image from the webcam
+      if webcam_num >= 0:
+        if not cap.isOpened():
+          raise RuntimeError("Webcam could not open. Please check connection.")
+        ret, frame = cap.read()
+        im_in = np.array(frame)
+      # Load the demo image
+      else:
+        im_file = os.path.join(args.image_dir, imglist[num_images])
+        # im = cv2.imread(im_file)
+        im_in = np.array(imread(im_file)) # height x width x 3
+      if len(im_in.shape) == 2:
+        im_in = im_in[:,:,np.newaxis]
+        im_in = np.concatenate((im_in,im_in,im_in), axis=2)
+      # rgb -> bgr
+      im = im_in[:,:,::-1]
+
+      blobs, im_scales = _get_image_blob(im)
+      assert len(im_scales) == 1, "Only single-image batch implemented"
+      im_blob = blobs
+      im_info_np = np.array([[im_blob.shape[1], im_blob.shape[2], im_scales[0]]], dtype=np.float32)
+
+      im_data_pt = torch.from_numpy(im_blob)
+      im_data_pt = im_data_pt.permute(0, 3, 1, 2)
+      im_info_pt = torch.from_numpy(im_info_np)
+
+      im_data.data.resize_(im_data_pt.size()).copy_(im_data_pt)
+      im_info.data.resize_(im_info_pt.size()).copy_(im_info_pt)
+      gt_boxes.data.resize_(1, 1, 5).zero_()
+      num_boxes.data.resize_(1).zero_()
+
+      # pdb.set_trace()
+      det_tic = time.time()
+
+      rois, cls_prob, bbox_pred, \
+      rpn_loss_cls, rpn_loss_box, \
+      RCNN_loss_cls, RCNN_loss_bbox, \
+      rois_label = fasterRCNN(im_data, im_info, gt_boxes, num_boxes)
+
+      scores = cls_prob.data[:, :max_per_image, :]
+      boxes = rois.data[:, :max_per_image, 1:5] # number of proposals after nms is 300, pre-nms is 6000 (see config.py), ranked by objectness score
+
+      # print(rois.size(), cls_prob.size(), bbox_pred.size()) # (1L, 300L, 5L) (1L, 300L, 81L) (1L, 300L, 324L)
+      # print(rois[:, 1:5, :])
+
+      # normalize coordinates to 0-1
+      # 800 is the scale (large scale coco)
+      assert boxes.size(0) == 1
+      width = 800.*max(1, im_in.shape[1]/im_in.shape[0])
+      height = 800.*max(1, im_in.shape[0]/im_in.shape[1])
+
+      boxes = boxes.squeeze(0)
+      boxes[:, 0] = (boxes[:, 0]+0.5)/width
+      boxes[:, 2] = (boxes[:, 2]+0.5)/width
+      boxes[:, 1] = (boxes[:, 1]+0.5)/height
+      boxes[:, 3] = (boxes[:, 3]+0.5)/height
+
+      results.append(boxes)
+
+      print(torch.max(boxes), imglist[num_images], im_in.shape)
+      assert len(torch.nonzero(boxes>1).view(-1)) == 0
+
+      if webcam_num == -1:
+        num_images -= 1
+
+  with open(args.output_lst, 'w') as f:
+      f.write(', '.join(imglist[::-1]))
+
+  torch.save(torch.stack(results), args.output_chunk)
